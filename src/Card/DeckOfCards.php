e8a0a388b022617167180452397d82075156fa6a
wiis22 <wiis22@student.bth.se>
2024-04-14 18:32:30 +0200

    trying to add utf8 ceracters for the cards

--- src/Card/DeckOfCards.php
+++ src/Card/DeckOfCards.php
@@ -17,7 +17,7 @@ class DeckOfCards
     public function initDeck(): void
     {
         $suits = ["♠", "♥", "♦", "♣"];
-        $values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+        $values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
         foreach ($suits as $suit) {
             foreach ($values as $value) {
