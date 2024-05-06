ce8bc38d182e515ec9caa8cdf47a1c0705253c2d
wiis22 <wiis22@student.bth.se>
2024-05-06 23:20:40 +0200

    Added so you can update info in a book

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -87,40 +87,70 @@ class LibraryController extends AbstractController
     #[Route('/library/update/{id}', name: 'update_book_form')]
     public function update( LibraryRepository $libraryRepository, int $id ): Response
     {
-        return $this->render('library/update.html.twig');
+        $book = $libraryRepository
+        ->find($id);
+
+        $data = [
+            'book' => $book
+        ];
+        return $this->render('library/update.html.twig', $data);
     }
 
-    #[Route('/library/update_book', name: 'update_book', methods: ['POST'])]
+    #[Route('/library/update_book/{id}', name: 'update_book', methods: ['POST'])]
     public function updateBook(
         Request $request,
-        ManagerRegistry $doctrine
+        ManagerRegistry $doctrine,
+        int $id
     ): Response {
 
         $entityManager = $doctrine->getManager();
+        $book = $entityManager->getRepository(Library::class)->find($id);
 
-        $bookPaths = array("img/bokbild1.png", "img/bokbild2.png", "img/bokbild3.png");
+        if (!$book) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
 
-        $title = $request->request->get('title');
-        $isbn = $request->request->get('isbn');
-        $author = $request->request->get('author');
-        $imgpath = $bookPaths[rand(0,2)];
+        $title = $request->request->get('title2');
+        $isbn = $request->request->get('isbn2');
+        $author = $request->request->get('author2');
 
-        $book = new Library();
         $book->setTitel($title);
         $book->setISBN($isbn);
         $book->setAuthor($author);
-        $book->setPicture($imgpath);
 
-        $entityManager->persist($book);
 
         $entityManager->flush();
 
         $this->addFlash(
             'notice',
-            'Boken finns nu i bibloteket!'
+            'Boken är nu uppdaterad!'
         );
-        return $this->render('library/index.html.twig');
+        return $this->redirectToRoute('show_books');
     }
 
+    #[Route('/library/delete/{id}', name: 'book_delete_by_id')]
+    public function deleteBookById(
+        ManagerRegistry $doctrine,
+        int $id
+    ): Response {
+        $entityManager = $doctrine->getManager();
+        $book = $entityManager->getRepository(Library::class)->find($id);
+
+        if (!$book) {
+            throw $this->createNotFoundException(
+                'No book found for id '.$id
+            );
+        }
+
+        $entityManager->remove($book);
+        $entityManager->flush();
+        $this->addFlash(
+            'warning',
+            'Boken är nu borttagen!'
+        );
+        return $this->redirectToRoute('show_books');
+    }
 
 }
